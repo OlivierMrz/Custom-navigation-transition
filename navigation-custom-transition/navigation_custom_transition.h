9e1595336f09adb06781b4e9f2c880e4817ad7d4
Olivier Miserez <olivierm@avio.aero>
2021-05-18 10:19:59 +0200

    Initial Commit

---
+++ navigation-custom-transition/navigation_custom_transition.h
@@ -0,0 +1,18 @@
+//
+//  navigation_custom_transition.h
+//  navigation-custom-transition
+//
+//  Created by Olivier Miserez on 18/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for navigation_custom_transition.
+FOUNDATION_EXPORT double navigation_custom_transitionVersionNumber;
+
+//! Project version string for navigation_custom_transition.
+FOUNDATION_EXPORT const unsigned char navigation_custom_transitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <navigation_custom_transition/PublicHeader.h>
+
+
